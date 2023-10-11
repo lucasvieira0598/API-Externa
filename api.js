545a66f5183058a4684ee5a59de14a693bd2756d
lucasvieira0598 <142995002+lucasvieira0598@users.noreply.github.com>
2023-10-11 09:24:12 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,10 @@
+let Conteudo_Pagina = document.getElementById("Conteudo")
 fetch("https://mocki.io/v1/8f1dfb1a-ecff-456c-837a-204f2de94b34")
 .then(Response => Response.json())
 .then(json => {
-    console.log(json)
+    //console.log(json)
+    json.forEach(element => {
+        console.log(element.name)
+        
+    });
 })
