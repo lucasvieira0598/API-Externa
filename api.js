4e25d08b1337b6cbf987aced9d048c5ccf1002ac
lucasvieira0598 <142995002+lucasvieira0598@users.noreply.github.com>
2023-10-11 09:45:58 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,9 +1,14 @@
+import Usuario from "./usuarios"
+
+let usuario_elemento = new Usuario()
+
 let Conteudo_Pagina = document.getElementById("Conteudo")
 fetch("https://mocki.io/v1/8f1dfb1a-ecff-456c-837a-204f2de94b34")
 .then(Response => Response.json())
 .then(json => {
     //console.log(json)
     json.forEach(element => {
+        usuario_elemento.CriarUsuario(element.name,element.bio,element.language,element.id,Conteudo_Pagina)
         console.log(element.name)
         
     });
