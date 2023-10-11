38a9cd8a8ab8ab71a41367b31a30b1d95f0e05f5
lucasvieira0598 <142995002+lucasvieira0598@users.noreply.github.com>
2023-10-11 09:45:26 -0300

    Add files via upload

---
+++ usuarios.js
@@ -0,0 +1,8 @@
+class Usuario{
+    CriarUsuario(bio,name, language, id, elemento){
+        elemento.innerHTML+="<div class='card'> <h1>"+""+name+"</h1><h3>"+id+"</h3>"+"<h4>"+bio+"</h4>"+
+        "<h4>"+language+"</h4>"
+    }
+}
+
+export default Usuario
